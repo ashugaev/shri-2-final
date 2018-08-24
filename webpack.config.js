407c157f6274ebb168d7a658ca6566a6676a6117
alek-2020 <alek-2020@yandex.ru>
2018-08-24 09:35:36 +0300

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
-// const path = require('path');
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: 'development',
     entry: './src/js/main.js', //path relative to this file
     output: {
+        path: path.resolve(__dirname, 'dist'),
         filename: 'app.bundle.js' //path relative to this file
     },
     module: {
-        rules: [{
+        rules: [
+            {
                 test: /\.scss$/,
-                use: [{
-                    loader: "style-loader"
-                }, {
-                    loader: "css-loader"
-                }, {
-                    loader: "sass-loader",
-                    options: {
-                        includePaths: ["absolute/path/a", "absolute/path/b"]
-                    }
-                }]
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it use publicPath in webpackOptions.output
+                            publicPath: '../'
+                        }
+                    },
+                    "css-loader"
+                ]
             },
             {
                 test: /\.(ttf|eot|svg|gif|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -29,8 +33,14 @@ module.exports = {
             }
         ]
     },
-    // plugins: [
-    //     new ExtractTextPlugin('style.css')
-    // ]
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
 
 };
