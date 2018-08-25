098323cc9aefc77d5e1e11c9d13106937a4ef815
alek-2020 <alek-2020@yandex.ru>
2018-08-25 14:48:52 +0300

    настроил минификатор

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,12 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
       },
       {
         test: /\.(png|jpg|gif)$/,
